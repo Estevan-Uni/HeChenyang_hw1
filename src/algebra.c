d6a82d0b0e5e8ba2db2f53f08a90061f5d2a6b78
Estevan-Uni <h3323224791@foxmail.com>
2025-04-23 19:15:06 +0800

    first version

--- src/algebra.c
+++ src/algebra.c
@@ -12,38 +12,120 @@ Matrix create_matrix(int row, int col)
 
 Matrix add_matrix(Matrix a, Matrix b)
 {
-    // ToDo
-    return create_matrix(0, 0);
+    if (a.rows!=b.rows || a.cols!=b.cols) {
+        printf("Error: Matrix a and b must have the same rows and cols.\n");
+        Matrix c=create_matrix(0,0);
+        return c;
+    }
+    else{
+        Matrix c=create_matrix(a.rows,a.cols);
+        for (int i=0;i<c.rows;i++) {
+            for (int j=0;j<c.cols;j++) {
+                c.data[i][j]=a.data[i][j]+b.data[i][j];
+            }
+        }
+        return c;    
+    }
+    
 }
-
 Matrix sub_matrix(Matrix a, Matrix b)
 {
-    // ToDo
-    return create_matrix(0, 0);
+    if (a.rows!=b.rows || a.cols!=b.cols) {
+        printf("Error: Matrix a and b must have the same rows and cols.\n");
+        Matrix c=create_matrix(0,0);
+        return c;
+    }
+    else{
+        Matrix c=create_matrix(a.rows,a.cols);
+        for (int i=0;i<c.rows;i++) {
+            for (int j=0;j<c.cols;j++) {
+                c.data[i][j]=a.data[i][j]-b.data[i][j];
+            }
+        }
+        return c;    
+    }
+    return create_matrix(0,0);
 }
 
 Matrix mul_matrix(Matrix a, Matrix b)
 {
-    // ToDo
-    return create_matrix(0, 0);
+    if (a.cols!=b.rows) {
+        printf("Error: The number of cols of matrix a must be equal to the number of rows of matrix b.\n");
+        Matrix c=create_matrix(0,0);
+        return c;
+    }
+    else{
+        Matrix c=create_matrix(a.rows,b.cols);
+        for (int i=0;i<c.rows;i++) {
+            for (int j=0;j<c.cols;j++) {
+                double c_num=0.0;
+                for (int k=0;k<a.cols;k++) {
+                    c_num +=a.data[i][k]*b.data[k][j];
+                }
+            c.data[i][j] = c_num;
+            }
+        }
+        return c;
+    }
 }
-
 Matrix scale_matrix(Matrix a, double k)
 {
-    // ToDo
-    return create_matrix(0, 0);
+    Matrix c=create_matrix(a.rows,a.cols);
+    for (int i=0;i<c.rows;i++) {
+        for (int j=0;j<c.cols;j++) {
+            c.data[i][j]=a.data[i][j]*k;
+        }
+    }
+    return c;
 }
 
 Matrix transpose_matrix(Matrix a)
 {
-    // ToDo
-    return create_matrix(0, 0);
+    Matrix c=create_matrix(a.cols,a.rows);
+    for (int i=0;i<a.rows;i++) {
+        for (int j=0;j<a.cols;j++) {
+            c.data[j][i]=a.data[i][j];
+        }
+    }
+    return c;
 }
 
 double det_matrix(Matrix a)
 {
-    // ToDo
-    return 0;
+    if (a.rows!=a.cols) {
+        printf("Error: The matrix must be a square matrix.\n");
+        return 0.0;
+    }
+    else if(a.rows==1&&a.cols==1){
+        return a.data[0][0];
+    }
+    else{
+        double sum=0.0;
+        double pn_num=1.0;
+        for (int i=0;i<a.rows;i++){
+            Matrix c=create_matrix(a.rows-1,a.cols-1);
+            for(int j=0;j<a.rows;j++){
+                if(i==j)continue;
+                for(int k=1;k<a.cols;k++){
+                    if(j<i){
+                        c.data[j][k-1]=a.data[j][k];
+                    }
+                    else{
+                        c.data[j-1][k-1]=a.data[j][k];
+                    }
+                }
+            }
+            double row_num=0.0;
+            row_num=a.data[i][0]*pn_num*det_matrix(c);
+            for (int i = 0; i < m.rows; i++) {
+                free(c.data[i]);
+            }
+            free(c.data);
+            sum+=row_num;
+            pn_num=-pn_num;
+        }
+        return sum;
+    }
 }
 
 Matrix inv_matrix(Matrix a)
@@ -54,16 +136,58 @@ Matrix inv_matrix(Matrix a)
 
 int rank_matrix(Matrix a)
 {
-    // ToDo
-    return 0;
+    Matrix c=create_matrix(a.rows,a.cols);
+    for(int i=0;i<a.rows;i++) {
+        for(int j=0;j<a.cols;j++) {
+            c.data[i][j]=a.data[i][j];
+        }
+    }
+    int row=0;
+    for(int ii=0;ii<a.cols&&row<a.rows;ii++) {
+        int Num=row;
+        for(int jj=row+1;jj<a.rows;jj++) {
+            if(fabs(c.data[jj][ii])>fabs(c.data[Num][ii])) Num=jj;
+        }
+        if(fabs(c.data[Num][ii])==0)continue;
+        if(Num!=row){
+            for(int z=0;z<a.cols;z++){
+                double t=c.data[row][z];
+                c.data[row][z]=c.data[Num][z];
+                c.data[Num][z]=t;
+            }
+        }
+        double xx=c.data[row][ii];
+        for(int k=ii;k<a.cols;k++)c.data[row][j]/=xx;
+        for(int kk=0;kk<a.rows;kk++){
+            if(kk!=row){
+                double mul=c.data[kk][ii];
+                for(int j=col;j<n;j++){
+                    m2[kk][k]-=mul*c.data[row][k];
+                }
+            }
+        }
+        row++;
+    }
+    int rank=row;
+    for (int i = 0; i < m.rows; i++) {
+        free(c.data[i]);
+    }
+    free(c.data);
+    return rank;
 }
 
 double trace_matrix(Matrix a)
 {
-    // ToDo
-    return 0;
+    if (a.rows!=a.cols) {
+        printf("Error: The matrix must be a square matrix.\n");
+        return 0.0;
+    }
+    double sum=0.0;
+    for (int i=0;i<a.rows;i++) {
+        sum+=a.data[i][i];
+    }
+    return sum;
 }
-
 void print_matrix(Matrix a)
 {
     for (int i = 0; i < a.rows; i++)
